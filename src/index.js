10992302f794dc9c4e802c67b95d4aadf47805fe
Sergei Panfilov <s.panfilov@maximatelecom.ru>
2016-11-16 21:26:23 +0300

    working react-router

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Libs
+import React from 'react';
+import { render } from 'react-dom';
+
+// CSS
+import './css/style.css';
+
+//Routes
+import Routes from './router';
+
+// Render
+render(Routes, document.getElementById('root')
+);
---
+++ src/router.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Router, Route, browserHistory, IndexRedirect} from 'react-router';
+
+// Components
+import App from './components/App';
+import Home from './components/Home';
+import About from './components/About';
+import Teachers from './components/Teachers';
+import Courses from './components/Courses';
+
+import CSS from './components/courses/CSS';
+import HTML from './components/courses/HTML';
+import Javascript from './components/courses/Javascript';
+
+// Routes
+
+const Routes = (
+  <Router history={browserHistory}>
+    <Route component={App}>
+      <Route path="/" component={Home} />
+      <Route path="about" component={About} />
+      <Route path="teachers" component={Teachers} />
+
+      <Route path="courses" component={Courses}>
+        <IndexRedirect to="html"/>
+        <Route path="html" component={HTML} />
+        <Route path="css" component={CSS} />
+        <Route path="javascript" component={Javascript} />
+      </Route>
+    </Route>
+  </Router>
+);
+
+export default Routes;
